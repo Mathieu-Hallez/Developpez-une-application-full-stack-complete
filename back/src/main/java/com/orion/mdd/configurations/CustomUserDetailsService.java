eb11bfb44eba89c7137e12407ac5e107aaeb994e
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-03-25 09:53:41 +0100

    Create custom user loader

---
+++ back/src/main/java/com/orion/mdd/configurations/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.orion.mdd.configurations;
+
+import com.orion.mdd.models.User;
+import com.orion.mdd.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.getUser(email);
+        if(user == null) {
+            throw new UsernameNotFoundException("No user with email " + email + "find.");
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getGrantedAuthorities("USER"));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authorities;
+    }
+}
