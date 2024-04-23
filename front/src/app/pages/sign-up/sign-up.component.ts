569e15281163f8165b8647d013cb65f644269452
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-04-23 17:45:42 +0200

    Implement input component and style of template

--- front/src/app/pages/sign-up/sign-up.component.ts
+++ front/src/app/pages/sign-up/sign-up.component.ts
@@ -1,3 +1,5 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
@@ -9,19 +11,36 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class SignUpComponent implements OnInit {
 
   signForm! : FormGroup;
+  isSmall : boolean = false;
 
-  constructor(private formBuilder : FormBuilder) { }
+  constructor(
+    private formBuilder : FormBuilder,
+    private location : Location,
+    private breakpointObserver : BreakpointObserver,
+  ) { }
 
   ngOnInit(): void {
     this.signForm = this.formBuilder.group({
       username: [null],
       email: [null],
       password: [null]
-    })
+    });
+
+    
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.HandsetPortrait
+    ]).subscribe(result => {
+        this.isSmall = result.matches;
+    });
   }
 
   onSubmitForm() {
     console.log(this.signForm.value);
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
