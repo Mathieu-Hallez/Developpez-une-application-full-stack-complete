17238100f7110670210345966f8dcc85d46ac884
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-04-23 22:58:20 +0200

    Add error message for inputs

--- front/src/app/pages/sign-up/sign-up.component.ts
+++ front/src/app/pages/sign-up/sign-up.component.ts
@@ -1,32 +1,70 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RegisterRequest } from 'src/app/interfaces/RegisterRequest';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
 
-  signForm! : FormGroup;
+  private register$! : Subscription;
+
+  signForm : FormGroup = this.formBuilder.group({
+    username: [
+      '',
+      [
+        Validators.required
+      ]
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)
+      ]
+    ]
+  });
+
   isSmall : boolean = false;
+
+  errorMessage : string | null = null;
+
+  get usernameFormField(): AbstractControl<string> | null {
+    return this.signForm.get('username');
+  }
+
+
+  get passwordFormField(): AbstractControl<string> | null {
+    return this.signForm.get('password');
+  }
+
+  get emailFormField(): AbstractControl<string> | null {
+    return this.signForm.get('email');
+  }
 
   constructor(
     private formBuilder : FormBuilder,
     private location : Location,
     private breakpointObserver : BreakpointObserver,
+    private authService : AuthenticationService,
+    private router : Router
   ) { }
 
   ngOnInit(): void {
-    this.signForm = this.formBuilder.group({
-      username: [null],
-      email: [null],
-      password: [null]
-    });
-
-    
     this.breakpointObserver.observe([
       Breakpoints.XSmall,
       Breakpoints.HandsetPortrait
@@ -35,11 +73,21 @@ export class SignUpComponent implements OnInit {
     });
   }
 
-  onSubmitForm() {
-    console.log(this.signForm.value);
+  ngOnDestroy(): void {
+    this.register$?.unsubscribe();
   }
 
-  goBack() {
+  onSubmitForm(): void {
+    const registerRequest = this.signForm.value as RegisterRequest;
+    console.log(registerRequest);
+    console.log(this.signForm.errors);
+    this.register$ = this.authService.register(registerRequest).subscribe({
+      next: _ => this.router.navigateByUrl('/sign-in'),
+      error: error => console.log(JSON.stringify(error))
+    })
+  }
+
+  goBack(): void {
     this.location.back();
   }
 
