ada880e0841cbb2d9a604c7732bfe5a0c941c50d
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-04-27 16:06:05 +0200

    Create login api call

--- front/src/app/services/api/authentification/authentication.service.ts
+++ front/src/app/services/api/authentification/authentication.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RegisterRequest } from '../../../interfaces/RegisterRequest';
+import { RegisterRequest } from '../../../interfaces/requests/RegisterRequest';
+import { LoginRequest } from 'src/app/interfaces/requests/LoginRequest';
+import { TokenDto } from 'src/app/interfaces/responses/TokenDto';
+import { ApiResponseDto } from 'src/app/interfaces/responses/ApiResponseDto';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,11 @@ export class AuthenticationService {
 
   constructor(private httpClient : HttpClient) { }
 
-  public register(registerRequest : RegisterRequest): Observable<void> {
-    return this.httpClient.post<void>(`${this.pathService}/register`, registerRequest);
+  public register(registerRequest : RegisterRequest): Observable<ApiResponseDto> {
+    return this.httpClient.post<ApiResponseDto>(`${this.pathService}/register`, registerRequest);
+  }
+
+  public login(loginRequest : LoginRequest): Observable<TokenDto> {
+    return this.httpClient.post<TokenDto>(`${this.pathService}/login`, loginRequest);
   }
 }
