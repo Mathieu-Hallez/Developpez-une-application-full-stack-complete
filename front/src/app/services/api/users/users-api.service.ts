3f2723c5b8a8a05ef21bd8b38f3b52b2956f1733
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-05-06 00:06:35 +0200

    Implement subscriptions end point

--- front/src/app/services/api/users/users-api.service.ts
+++ front/src/app/services/api/users/users-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PostDto } from 'src/app/interfaces/responses/PostDto';
+import { TopicDetailsDto } from 'src/app/interfaces/responses/TopicDetailsDto';
 import { TopicDto } from 'src/app/interfaces/responses/TopicDto';
 
 @Injectable({
@@ -15,5 +16,9 @@ export class UsersApiService {
 
   public allSubscriptionsPosts(): Observable<Array<PostDto>> {
     return this.httpClient.get<Array<PostDto>>(`${this.pathService}/subscriptions/posts`);
+  }
+
+  public subscriptions(): Observable<Array<TopicDetailsDto>> {
+    return this.httpClient.get<Array<TopicDetailsDto>>(`${this.pathService}/subscriptions`);
   }
 }
