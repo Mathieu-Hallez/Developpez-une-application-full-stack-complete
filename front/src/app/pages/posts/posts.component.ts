bc38446f84f6e74d2e495dc4ac4578b477f280c9
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-05-07 15:07:53 +0200

    Add order by for posts

--- front/src/app/pages/posts/posts.component.ts
+++ front/src/app/pages/posts/posts.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnDestroy } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 import { Observable, Subject, takeUntil } from 'rxjs';
 import { PostDto } from 'src/app/interfaces/responses/PostDto';
 import { UsersApiService } from 'src/app/services/api/users/users-api.service';
@@ -9,13 +10,13 @@ import { SessionService } from 'src/app/services/session.service';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent implements OnDestroy {
+export class PostsComponent implements OnDestroy, OnInit {
 
   private destroy$ : Subject<boolean> = new Subject<boolean>();
 
-  posts$ : Observable<PostDto[]> = this.usersApiService.allSubscriptionsPosts().pipe(takeUntil(this.destroy$));
+  posts : PostDto[] = [];
 
-  decreasingOrder : boolean = false;
+  descendingOrder : boolean = false;
 
   screenWidth : number = window.innerWidth;
 
@@ -23,6 +24,12 @@ export class PostsComponent implements OnDestroy {
     private usersApiService : UsersApiService,
     private sessionService : SessionService
   ) { }
+
+  ngOnInit(): void {
+    this.usersApiService.allSubscriptionsPosts().pipe(takeUntil(this.destroy$)).subscribe({
+      next: posts => this.posts = posts
+    });
+  }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
@@ -36,6 +43,20 @@ export class PostsComponent implements OnDestroy {
   onResize() {  
     this.screenWidth = window.innerWidth;
     console.log(this.screenWidth)
-  }  
+  }
 
+  switchOrderBy(): void {
+    this.descendingOrder = !this.descendingOrder;
+    this.posts = this.posts.sort(this.comparePost)
+  }
+
+  comparePost = (a : PostDto, b : PostDto): number => {
+    const dateA = dayjs(a.created_at);
+    const dateB = dayjs(b.created_at);
+    if(this.descendingOrder) {
+      return dateB.diff(dateA);
+    } else {
+      return dateA.diff(dateB);
+    }
+  }
 }
